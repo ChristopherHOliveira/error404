d421626bb91cc30a6bc2400beb2d7f7ad1c65bac
Christopher <christopher.h.oliveira@gmail.com>
2022-09-12 19:39:01 -0300

    commit

--- tickets01.py
+++ tickets01.py
@@ -1,24 +1,50 @@
 from flask import Flask
+import re
 
 app = Flask(__name__)
 
-usuarios = []       # lista onde serão armazenados os objetos 'usuários' (representação do BD):
-tickets = []        # lista onde serão armazenados os objetos 'tickets' (representação do BD):
+usuarios = []       # lista de objetos 'usuários' (representação do BD):
+tickets = []        # lista de objetos 'tickets' (representação do BD):
 
 class Usuario():                                                    # classe Usuário
-    def __init__(self, nome, email, username, senha, perfil):       # atributos do usuário
+    def __init__(self, nome, email, username, senha, perfil):       # parametros a serem passados como atributos do usuário
         self.nome = nome
         self.email = email
         self.username = username
         self.senha = senha
         self.perfil = perfil
 
-    def cadastrar_usuario(nome, email, username, senha, perfil):                # método para cadastrar usuário
-        usuarios.append(Usuario(nome, email, username, senha, perfil).__dict__) # passando o objeto usuário criado para a lista, já no formato dicionário
+    def cadastrar_usuario(nome, email, username, senha, perfil):           # método que cadastra usuário
+        if Usuario.valida_email(email) == True:                            # testando se e-mail é válido
+            usuarios.append(Usuario(nome, email, username, senha, perfil)) # passando o objeto usuário criado para a lista de usuários (BD)
+        else:
+            print(f'{email} é um email inválido')
 
-    @app.route('/usuarios')     # rota para exibir todos os usuários cadastrados
-    def exibir_usuarios():
-        return {'Usuarios': usuarios}
+    def valida_email(email_teste):                                 # método que valida email
+        regex_mail = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'           
+        if(re.search(regex_mail,email_teste)):  
+            return True                                            # e retorna True se o email é um e-mail válido   
+        else:  
+            return False
+
+    @app.route('/login/<username>/<senha>')
+    def valida_login(username, senha):                                   # método que valida login
+        for usuario in usuarios:                                         # verificando em cada usuário cadastrado
+            if usuario.username == username and usuario.senha == senha:  # se há um respectivo usuário e senha cadastrados
+                return '<h1>Usuário e senha corretos</h1>'
+                # acessar tela principal
+            else:
+                return '<h1>Usuário ou senha incorretos</h1>'
+                # permanecer na tela de login e exibir mensagem de erro
+
+    @app.route('/usuarios')
+    def exibir_usuarios():                          # método que exibe todos os usuários cadastrados
+        out_usuarios = []                           # criando lista pra output
+        for usuario in usuarios:                    # para cada usuário cadastrado
+            out_usuarios.append(usuario.__dict__)   # adicionando na lista de output em formato dicionário 
+        return {'Usuarios': out_usuarios}           # retornando a lista de output
+
+
 
 class Ticket(): # classe Ticket
     def __init__(self, titulo, descricao, prioridade):      # atributos do ticket
@@ -27,14 +53,17 @@ class Ticket(): # classe Ticket
         self.prioridade = prioridade
 
     def cadastrar_ticket(titulo, descricao, prioridade):                    # método para cadastrar ticket
-        tickets.append(Ticket(titulo, descricao, prioridade).__dict__)      # passando o objeto ticket criado para a lista, já no formato dicionário
+        tickets.append(Ticket(titulo, descricao, prioridade))      # passando o objeto ticket criado para a lista
 
     def pesquisar_ticket():
         pass
     
     @app.route('/tickets')      # rota para exibir todos os tickets cadastrados
     def exibir_tickets():
-        return {'Tickets': tickets}
+        out_tickets = []
+        for ticket in tickets:
+            out_tickets.append(ticket.__dict__)
+        return {'Tickets': out_tickets}
 
     @app.route('/tickets/pesq/<termo>')      # rota para pesquisar ticket que contenha determinado termo
     def pesquisar_ticket(termo):             # método que recebe o termo a ser pesquisado como parâmetro
@@ -47,7 +76,7 @@ class Ticket(): # classe Ticket
 
 # teste: cadastrando usuários e adicionando na lista 'usuarios':
 Usuario.cadastrar_usuario('Zohan', 'zohan@error404.com', 'z.salam', 'zoh123', 'admin')
-Usuario.cadastrar_usuario('Chris', 'chris@error404.com', 'c.olive', 'chr123', 'logistica')
+Usuario.cadastrar_usuario('Chris', 'emailerrado#provedor.com', 'c.olive', 'chr123', 'logistica')
 Usuario.cadastrar_usuario('Erica', 'erica@error404.com', 'e.olive', 'eri123', 'vendas')
 Usuario.cadastrar_usuario('João', 'joao@error404.com', 'j.silva', 'joa123', 'marketing')
 
